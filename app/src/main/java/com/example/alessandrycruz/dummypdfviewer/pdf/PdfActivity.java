ac39e88eff7f8e62cb56210a60ca907706a1014f
Alessandry Cruz <alessandrycruz@gmail.com>
2017-07-06 15:55:57 -0400

    Initial commit

--- app/src/main/java/com/example/alessandrycruz/dummypdfviewer/pdf/PdfActivity.java
+++ app/src/main/java/com/example/alessandrycruz/dummypdfviewer/pdf/PdfActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.pdf.PdfRenderer;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -25,23 +27,20 @@ import com.example.alessandrycruz.dummypdfviewer.pdf.utils.PdfRender_Util;
 public class PdfActivity extends Activity {
     public static final String PDF_ACTIVITY_EXTRAS_KEY = "pdf_activity_extras_key";
 
-    private static int ZERO = 0;
-    private static int ONE = 1;
-    private static int TWO = 2;
-
     private int mPdfPageCount;
     private int mCurrentPdfPage;
 
     private Button mButtonPrevious;
     private Button mButtonNext;
     private Context mContext;
+    private GestureDetector mGestureDetector;
     private ImageView mImageViewPdf;
     private PdfBase_Util mPdfBase_Util;
     private PdfRender_Util mPdfRender_Util;
     private PdfRenderer mPdfRenderer;
     private String mPdfFileName;
     private Pdf_Taskset mPdf_Taskset;
-    private ProgressBar mProgressBar;
+    private ProgressBar mProgressBarPdf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +48,7 @@ public class PdfActivity extends Activity {
         setContentView(R.layout.activity_pdf);
 
         initializeGlobalMembers();
+        initializeGestureDetector();
         getIntentExtras();
     }
 
@@ -75,6 +75,35 @@ public class PdfActivity extends Activity {
         }
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return mGestureDetector.onTouchEvent(event);
+    }
+
+    private GestureDetector.SimpleOnGestureListener mSimpleOnGestureListener
+            = new GestureDetector.SimpleOnGestureListener() {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            final float SENSITIVITY = 50;
+
+            if ((e1.getX() - e2.getX()) > SENSITIVITY) {
+                // LEFT Swipe
+                goToNextPage();
+            } else if ((e2.getX() - e1.getX()) > SENSITIVITY) {
+                // RIGHT Swipe
+                goToPreviousPage();
+            }
+
+            if ((e1.getY() - e2.getY()) > SENSITIVITY) {
+                // UP Swipe
+            } else if ((e2.getY() - e1.getY()) > SENSITIVITY) {
+                // DOWN Swipe
+            }
+
+            return super.onFling(e1, e2, velocityX, velocityY);
+        }
+    };
+
     public void onClickPrevious(View view) {
         goToPreviousPage();
     }
@@ -95,7 +124,11 @@ public class PdfActivity extends Activity {
         mPdfRender_Util = new PdfRender_Util();
         mImageViewPdf = findViewById(R.id.image_view_pdf);
         mPdf_Taskset = new Pdf_Taskset();
-        mProgressBar = findViewById(R.id.progress_bar_pdf);
+        mProgressBarPdf = findViewById(R.id.progress_bar_pdf);
+    }
+
+    private void initializeGestureDetector() {
+        mGestureDetector = new GestureDetector(mSimpleOnGestureListener);
     }
 
     private void getIntentExtras() {
@@ -107,6 +140,10 @@ public class PdfActivity extends Activity {
     }
 
     private void goToPreviousPage() {
+        final int ZERO = 0;
+        final int ONE = 1;
+        final int TWO = 2;
+
         if (mCurrentPdfPage <= ZERO) {
             mCurrentPdfPage = ZERO;
         } else {
@@ -128,6 +165,8 @@ public class PdfActivity extends Activity {
     }
 
     private void goToNextPage() {
+        final int ONE = 1;
+
         if (mCurrentPdfPage >= (mPdfPageCount - ONE)) {
             mCurrentPdfPage = mPdfPageCount - ONE;
         } else {
@@ -146,13 +185,13 @@ public class PdfActivity extends Activity {
     }
 
     private void initializePdf() {
-        mProgressBar.setVisibility(View.VISIBLE);
+        mProgressBarPdf.setVisibility(View.VISIBLE);
 
         mPdf_Taskset.executePdf_CreateFileInputStream_Task(mContext, mPdfFileName,
                 new Pdf_CreateFileInputStream_Listener() {
                     @Override
                     public void onSuccess(PdfRenderer pdfRenderer) {
-                        mProgressBar.setVisibility(View.GONE);
+                        mProgressBarPdf.setVisibility(View.GONE);
 
                         mPdfRenderer = pdfRenderer;
 
@@ -163,26 +202,26 @@ public class PdfActivity extends Activity {
 
                     @Override
                     public void onFail() {
-                        mProgressBar.setVisibility(View.GONE);
+                        mProgressBarPdf.setVisibility(View.GONE);
                     }
                 });
     }
 
     private void loadPdfPage() {
-        mProgressBar.setVisibility(View.VISIBLE);
+        mProgressBarPdf.setVisibility(View.VISIBLE);
 
         mPdf_Taskset.executePdf_ConvertPdfPageToBitmap(mContext, mPdfRenderer,
                 mCurrentPdfPage, new Pdf_ConvertPdfPageToBitmap_Listener() {
                     @Override
                     public void onSuccess(Bitmap bitmap) {
-                        mProgressBar.setVisibility(View.GONE);
+                        mProgressBarPdf.setVisibility(View.GONE);
 
                         mImageViewPdf.setImageBitmap(bitmap);
                     }
 
                     @Override
                     public void onFail() {
-                        mProgressBar.setVisibility(View.GONE);
+                        mProgressBarPdf.setVisibility(View.GONE);
                     }
                 });
     }
